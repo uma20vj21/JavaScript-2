5bf7e6356d4bb022be3d8976b1e08742c4e79a05
大嶋由真 <>
2022-06-08 23:41:23 +0900

    　修正完了

--- main.js
+++ main.js
@@ -1,7 +1,11 @@
-let question = prompt( '日本の首都は?' );
+const question = prompt( '日本の首都は?' );
 
-if (question = prompt( '日本の首都は?' ) === '東京') {
-  alert ( '正解です!' );
-} else {
-  alert ( '不正解です!' );
+while ( true ) {
+  if (question === '東京') {
+    alert ( '正解です!' );
+    break;
+  } else {
+    alert ( '不正解です!' );
+  }
 }
+
